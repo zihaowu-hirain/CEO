e3fe997192d34f22c75ef659512b4dec248a4c04
zihaowu <zihao.wu@igg.com>
2024-10-25 13:24:50 +0800

    函数序列化

--- ceo/action/action.py
+++ ceo/action/action.py
@@ -1,7 +1,7 @@
 import json
 from typing import Callable
 
-from ceo.prompt import generate_action_prompt
+from ceo.prompt import generate_action_prompt, generate_result_prompt
 
 
 class Action:
@@ -15,7 +15,8 @@ class Action:
         self.function: Callable = function
         self.parameters: dict = dict()
         for key, clazz in function.__annotations__.items():
-            self.parameters[key] = clazz.__name__
+            if key != 'return':
+                self.parameters[key] = clazz.__name__
 
     def __repr__(self):
         return json.dumps(obj={
@@ -25,18 +26,22 @@ class Action:
         }, ensure_ascii=False)
 
     def __str__(self):
-        return self.__repr__()
-
+        return generate_action_prompt(self)
 
     def invoke(self, **kwargs):
         return self.function(**kwargs)
 
+    def invoke_formatted(self, **kwargs):
+        return generate_result_prompt(
+            function_name=self.name,
+            result=self.invoke(**kwargs),
+            **kwargs
+        )
 
-# def test(a: int, b: float):
-#     return a + b
-#
-#
-# if __name__ == '__main__':
-#     print(Action('test', test).invoke(**{'a':1, 'b':2}))
-#     print(generate_action_prompt(Action('test', test)))
+    def invoke_from_string(self, param_str: str):
+        kwargs = json.loads(param_str)
+        return self.invoke(**kwargs)
 
+    def invoke_from_string_formatted(self, param_str: str):
+        kwargs = json.loads(param_str)
+        return self.invoke_formatted(**kwargs)
--- ceo/prompt/__init__.py
+++ ceo/prompt/__init__.py
@@ -1,5 +1,5 @@
-def generate_action_prompt(function: object):
-    return ('# Think if you need to use this function. '
+def generate_action_prompt(function: object) -> str:
+        return ('# Think if you need to use this function. '
             '\n- If you need this function, format your output into a json string '
             'which keys are the parameter names and the value is the value of each parameter. '
             '\n- If you dont need this function, output an empty json. '
@@ -8,3 +8,12 @@ def generate_action_prompt(function: object):
             '<Function>'
             f'{function.__repr__()}'
             '</Function>\n')
+
+
+def generate_result_prompt(function_name: str, result: str, **kwargs) -> str:
+        param = str()
+        for key, value in kwargs.items():
+                param += f'{key}={value},'
+        param = param[:-1]
+        return f'{function_name}({param})->{result}\n'
+
