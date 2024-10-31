6c9b01a93a9d5334088222250943fde8fcf798cf
吴子豪 <vortezwohl@proton.me>
2024-11-01 02:58:33 +0800

    Agent: 将function表述改为ability，提供赋予能力和剥夺能力方法，规定repr格式，为Agent增加名称字段

--- ceo/brain/agent.py
+++ ceo/brain/agent.py
@@ -13,21 +13,66 @@ from ceo.prompt import (
 )
 
 log = logging.getLogger('ceo')
+DEFAULT_NAME = 'CEO (Default)'
 
 
 class Agent:
-    def __init__(self, functions: list[Callable], model: BaseChatModel, query: str = '', ext_context: str = ''):
+    def __init__(self, abilities: list[Callable], brain: BaseChatModel, name: str = DEFAULT_NAME, query: str = '', ext_context: str = ''):
         self.actions = list()
         self.prev_results = list()
         self.schedule = list()
         self.act_count = 0
-        self.model = model
+        self.name = name
+        self.model = brain
         self.ext_context = ext_context
         self.query_high_level, self.query_by_step = (
             QueryResolverPrompt(query=query, ext_context=ext_context).invoke(self.model)
         )
-        for function in functions:
-            self.actions.append(Action(function))
+        for ability in abilities:
+            self.actions.append(Action(ability))
+
+    def __repr__(self):
+        actions_str = '['
+        for action in self.actions:
+            actions_str += f'{action.name}, '
+        actions_str = actions_str[:-2] + ']'
+        if actions_str == ']':
+            actions_str = '[]'
+        schedule_str = '['
+        for action in self.schedule:
+            schedule_str += f'{action.name}, '
+        schedule_str = schedule_str[:-2] + ']'
+        if schedule_str == ']':
+            schedule_str = '[]'
+        ext_context = self.ext_context
+        if ext_context == '':
+            ext_context = 'None'
+        return (f'Agent: \n'
+                f'- Name: {self.name}\n'
+                f'- Brain: {self.model.dict()['model_name']}\n'
+                f'- Abilities: {actions_str}\n'
+                f'- Schedule: {schedule_str}\n'
+                f'- ExternalContext: {ext_context}')
+
+    def __str__(self):
+        return self.__repr__()
+
+    def grant_ability(self, ability: Callable):
+        self.actions.append(Action(ability))
+
+    def grant_abilities(self, abilities: list[Callable]):
+        for ability in abilities:
+            self.grant_ability(ability)
+
+    def deprive_ability(self, ability: Callable):
+        action = Action(ability)
+        for _action in self.actions:
+            if _action.name == action.name:
+                self.actions.remove(_action)
+
+    def deprive_abilities(self, abilities: list[Callable]):
+        for ability in abilities:
+            self.deprive_ability(ability)
 
     def plan(self) -> list:
         scheduling = SchedulerPrompt(query=self.query_by_step, actions=self.actions, ext_context=self.ext_context)
@@ -78,4 +123,4 @@ class Agent:
             ext_context=self.ext_context).invoke(self.model))
         log.debug(f'Conclusion: {response}')
         self.reposition()
-        return response
+        return f'{self.name}: response'
