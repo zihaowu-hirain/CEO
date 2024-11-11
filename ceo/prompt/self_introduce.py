222a532cdef12b8d26cd5b951a5b7f1fe333c790
zihaowu <zihao.wu@igg.com>
2024-11-11 14:02:59 +0800

    升级版本到 0.6.2-preview，解决agent循环调用的bug

--- ceo/prompt/self_introduce.py
+++ ceo/prompt/self_introduce.py
@@ -3,14 +3,13 @@ from collections.abc import Iterator
 
 from langchain_core.language_models import BaseChatModel
 
-from ceo.brain.agent import Agent
 from ceo.prompt.prompt import Prompt
 
 log = logging.getLogger('ceo.prompt')
 
 
 class SelfIntroducePrompt(Prompt):
-    def __init__(self, agent: Agent, ext_context: str = ''):
+    def __init__(self, agent: any, ext_context: str = ''):
         prompt = ('Task: Introduce yourself briefly base on below information.\n'
                   f'Information: {agent.__repr__()}\n'
                   f'Output format: My name is <name>. What can i do: ...\n')
