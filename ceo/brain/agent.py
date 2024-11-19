30441260d0d574787e49903b2b3475cddf2ca299
zihaowu <zihao.wu@igg.com>
2024-11-19 14:59:55 +0800

    改进日志以适配多agent

--- ceo/brain/agent.py
+++ ceo/brain/agent.py
@@ -89,7 +89,7 @@ class Agent:
     def plan(self) -> list:
         scheduling = SchedulerPrompt(query=self.query_by_step, actions=self.abilities, ext_context=self.ext_context)
         self.schedule = scheduling.invoke(self.model)
-        log.debug(f'Schedule: {[_.name for _ in self.schedule]}. Query: "{self.query_high_level}".')
+        log.debug(f'Agent: {self.name}, Schedule: {[_.name for _ in self.schedule]}. Query: "{self.query_high_level}".')
         return self.schedule
 
     def reposition(self):
@@ -116,7 +116,7 @@ class Agent:
             )
             action, params = analysing.invoke(self.model)
             executing = ExecutorPrompt(params=params, action=action, ext_context=self.ext_context)
-            action_str = f'Action {self.act_count + 1}/{len(self.schedule)}: {executing.invoke(model=self.model)}'
+            action_str = f'Agent: {self.name}, Action {self.act_count + 1}/{len(self.schedule)}: {executing.invoke(model=self.model)}'
             self.prev_results.append(action_str)
             self.act_count += 1
             log.debug(action_str)
@@ -133,6 +133,6 @@ class Agent:
             query=self.query_high_level,
             prev_results=self.prev_results,
             ext_context=self.ext_context).invoke(self.model))
-        log.debug(f'Conclusion: {response}')
+        log.debug(f'Agent: {self.name}, Conclusion: {response}')
         self.reposition()
         return f'{self.name}: {response}'
