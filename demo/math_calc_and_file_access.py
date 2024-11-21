6917ff60600b136fc19ba819471618d0cc1fe7e8
zihaowu <zihao.wu@igg.com>
2024-11-21 18:07:36 +0800

    更新 demo

--- demo/math_calc_and_file_access.py
+++ demo/math_calc_and_file_access.py
@@ -1,5 +1,3 @@
-import logging
-
 from ceo import Agent, get_openai_model
 from sympy import simplify
 from dotenv import load_dotenv
@@ -7,10 +5,6 @@ from dotenv import load_dotenv
 from ceo.util import ability
 
 load_dotenv()
-log = logging.getLogger("ceo")
-log.setLevel(logging.DEBUG)
-# log = logging.getLogger("ceo.ability")
-# log.setLevel(logging.DEBUG)
 
 
 @ability
@@ -25,6 +19,8 @@ def write_file(filename: str, content: str) -> bool:
     return True
 
 
-ceo = Agent(abilities=[constant_calculate, write_file], brain=get_openai_model(), name='test')
-ceo.assign("Here is a sphere with radius of 3.1121 cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
-result = ceo.just_do_it()
+if __name__ == '__main__':
+    ceo = Agent(abilities=[constant_calculate, write_file], brain=get_openai_model(), name='test')
+    ceo.assign("Here is a sphere with radius of 3.1121 cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
+    result = ceo.just_do_it()
+    print(result)
--- demo/multi_agent.py
+++ demo/multi_agent.py
@@ -1,4 +1,3 @@
-import logging
 import sys
 
 import sympy
@@ -8,16 +7,9 @@ from ceo import Agent, get_openai_model
 from ceo.util import agentic, ability
 
 load_dotenv()
-log = logging.getLogger("ceo")
-log.setLevel(logging.DEBUG)
-# log = logging.getLogger("ceo.ability")
-# log.setLevel(logging.DEBUG)
-# log = logging.getLogger('ceo.agent')
-# log.setLevel(logging.DEBUG)
+sys.set_int_max_str_digits(10**8)
 
 model = get_openai_model()
-
-sys.set_int_max_str_digits(10**8)
 
 
 @ability
@@ -51,7 +43,8 @@ def agent2():
 
 
 if __name__ == '__main__':
-    agent = Agent(abilities=[agent1, agent2], brain=get_openai_model(), name='test')
-    agent.assign("Here is a sphere with a radius of 9.987 cm and pi here is 3.14159, "
+    agent = Agent(abilities=[agent1, agent2], brain=model, name='test')
+    result = agent.assign("Here is a sphere with a radius of 9.987 cm and pi here is 3.14159, "
                  "find the area and volume respectively, "
                  "then write the results into a file called 'result.txt'.").just_do_it()
+    print(result)
