c7fc19591c6d978e0ff25e49e3cc7ed87e569d7b
吴子豪 <vortezwohl@proton.me>
2024-11-29 00:14:17 +0800

    fix: agentic.py reformatted

--- ceo/util/agentic.py
+++ ceo/util/agentic.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from ceo import Agent
@@ -10,16 +11,28 @@ def agentic(agent: Agent):
         def wrapper(query: str, *args, **kwargs) -> str:
             return agent.assign(query).just_do_it()
         wrapper.__name__ = f'talk_to_{agent.name}'
-        wrapper.__doc__ = (f'Initiates a conversation with "{agent.name}" to use its abilities.\n'
-                        f'First, carefully consider and explore {agent.name}\'s potential abilities in solving your tasks, '
-                        f'then, if you need {agent.name}\'s help, you must tell detailed and exactly what you need {agent.name} to do.\n'
-                        f'\nSelf introduction from {agent.name}: "{agent.introduction}".\n'
-                        f'\nBelow is detailed information about {agent.name}:\n'
-                        f'{"-"*6}\n{agent}\n{"-"*6}\n'
-                        f'\nArgs:\n'
-                        f'query (str): The input query to be processed by {agent.name}.\n'
-                        f'\nReturns:\n'
-                        f'str: {agent.name}\'s response to the query.\n')
+        wrapper.__doc__ = json.dumps({
+            "description": {
+                "brief_description": f'Initiates a conversation with "{agent.name}" to use its abilities.',
+                "detailed_description": f"First, carefully consider and explore {agent.name}'s potential abilities in solving your tasks, "
+                                        f"then, if you need {agent.name}'s help, you must tell comprehensively, precisely "
+                                        f"and exactly what you need {agent.name} to do.",
+                f"self_introduction_from_{agent.name}": agent.introduction,
+                f"metadata_of_{agent.name}": agent.__repr__(),
+                "args": [{
+                    "query": {
+                      "name": "query",
+                      "type": "str",
+                      "description": f"a comprehensively, precisely and exactly instruction to be processed by {agent.name}."
+                    }
+                  }
+                ],
+                "returns": {
+                  "type": "str",
+                  "description": f"{agent.name}'s response to your instruction."
+                }
+            }
+        }, ensure_ascii=False)
         log.debug(f'Agent dispatcher generated. {wrapper.__name__}: {wrapper.__doc__}')
         return wrapper
     return decorator
