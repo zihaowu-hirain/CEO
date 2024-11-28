23ab639e54849f85d65b1ac89e1f9c6d74ad4199
吴子豪 <vortezwohl@proton.me>
2024-11-28 23:59:01 +0800

    update demo

--- demo/math_calc_and_file_access.py
+++ demo/math_calc_and_file_access.py
@@ -21,6 +21,6 @@ def write_file(filename: str, content: str) -> bool:
 
 if __name__ == '__main__':
     ceo = Agent(abilities=[constant_calculate, write_file], brain=get_openai_model(), name='test')
-    ceo.assign("Here is a sphere with radius of 3.1121 cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
+    ceo.assign("Here is a sphere with radius of 5.1121 cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
     result = ceo.just_do_it()
     print(result)
