3d96d5dad5855132b0efefb69391b252c092878e
吴子豪 <vortezwohl@proton.me>
2024-11-28 22:10:41 +0800

    fix: executor_prompt.py 格式优化

--- ceo/prompt/executor_prompt.py
+++ ceo/prompt/executor_prompt.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from collections.abc import Iterator
 
@@ -13,14 +14,16 @@ class ExecutorPrompt(Prompt):
     def __init__(self, params: dict, action: Ability, ext_context: str = ''):
         self.action = action
         self.params = params
-        prompt = ('Precondition: Below is a tool and your choice(params) for the tool.\n'
-                  'Task: Explain what you are going to do.\n'
-                  'Output format: text.\n'
-                  'Example output: I am trying to open calculator.\n'
-                  f'Tool: {action}\n'
-                  f'Params(choice): {params}\n')
+        prompt = json.dumps({
+            "precondition": "Below is a tool and your choice (params) for using this tool.",
+            "task": "Explain what you are going to do.",
+            "output_format": "text",
+            "output_example": "I am trying to open calculator.",
+            "tool": action.__repr__(),
+            "params (choice)": params
+        }, ensure_ascii=False)
         super().__init__(prompt, ext_context)
-        log.debug(f'Executor prompt(before): {self.prompt}')
+        log.debug(f'ExecutorPrompt (before): {self.prompt}')
 
     def explain(self, model: BaseChatModel, stream: bool = False) -> str | Iterator:
         if stream:
@@ -29,18 +32,25 @@ class ExecutorPrompt(Prompt):
 
     def invoke(self, model: BaseChatModel, stream: bool = False) -> str | Iterator:
         result = self.action.function(**self.params)
-        prompt = ('Precondition: Below is a tool, your choice(params) for the tool, '
-                  'and the result of your using the tool.\n'
-                  'Task: Explain what you have done, and write down the result detailed. '
-                  '(The result is shown below at [Result])\n'
-                  'Output format: text.\n'
-                  'Output contains: {the_choice_you_made}, {what_you_have_done}\n'
-                  'Output example: I wrote a wechat message which says "Bonjour". The result is "success"\n'
-                  f'Tool: {self.action}\n'
-                  f'Params(choice): {self.params}\n'
-                  f'Result: {result}\n')
+        prompt = json.dumps({
+            "precondition": "Below is a tool, your choice (params) for the tool, "
+                            "and the result of your using of this tool.",
+            "task": "Explain what you have done, and write down the result detailed. "
+                    "The result is shown below at [result].",
+            "output_format": "text",
+            "output_contains": [
+                "{the_tool(ability)_you_used}",
+                "{the_choice_you_made}",
+                "{what_you_have_done}"
+            ],
+            "hint_for_output": 'When you give the response, say "ability" instead of "tool".',
+            "output_example": "I wrote a wechat message which says 'Bonjour'. The result is 'success'.",
+            "tool": self.action,
+            "params (choice)": self.params,
+            "result": result
+        }, ensure_ascii=False)
         prompt = f'{self.ext_context}{self.seperator}{prompt}'
-        log.debug(f'Executor prompt(after): {prompt}')
+        log.debug(f'ExecutorPrompt (after): {prompt}')
         if stream:
             return model.stream(prompt)
         return model.invoke(prompt).content
