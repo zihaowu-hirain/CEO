e555f06a19f95f50f099728e65e3ddace9144291
吴子豪 <vortezwohl@proton.me>
2024-12-01 07:10:03 +0800

    update agent.py 优化封装

--- ceo/brain/agent.py
+++ ceo/brain/agent.py
@@ -19,39 +19,32 @@ log = logging.getLogger('ceo')
 
 class Agent:
     def __init__(self, abilities: list[Callable], brain: BaseChatModel, name: str, query: str = ''):
-        self.abilities = list()
-        self.prev_results = list()
-        self.schedule = list()
-        self.act_count = 0
-        self.name = name
-        self.model = brain
-        self.query_high_level = self.query_by_step = str()
+        self._abilities = list()
+        self._act_count = 0
+        self._name = name
+        self._model = brain
+        self._query_high_level = self._query_by_step = str()
         if query is not None and query != '':
-            self.query_high_level, self.query_by_step = QueryResolverPrompt(query).invoke(self.model)
+            self._query_high_level, self._query_by_step = QueryResolverPrompt(query).invoke(self._model)
         for ability in abilities:
-            self.abilities.append(Ability(ability))
-        self.introduction = str()
+            self._abilities.append(Ability(ability))
+        self._introduction = str()
         self.introduce()
+        self.__prev_results = list()
+        self.__schedule = list()
 
     def __repr__(self):
         ability_str = '['
-        for ability in self.abilities:
+        for ability in self._abilities:
             ability_str += f'{ability}, '
         ability_str = ability_str[:-2] + ']'
         if ability_str == ']':
             ability_str = '[]'
-        schedule_str = '['
-        for ability in self.schedule:
-            schedule_str += f'{ability.name}, '
-        schedule_str = schedule_str[:-2] + ']'
-        if schedule_str == ']':
-            schedule_str = '[]'
         return json.dumps({
-            self.name: {
-                "name": self.name,
-                "brain": self.model.dict()['model_name'],
-                "abilities": ability_str,
-                "schedule": schedule_str
+            self._name: {
+                "name": self._name,
+                "brain": self._model.dict()['model_name'],
+                "abilities": ability_str
             }
         }, ensure_ascii=False)
 
@@ -59,12 +52,12 @@ class Agent:
         return self.__repr__()
 
     def introduce(self, update: bool = False) -> str:
-        if self.introduction == '' or update:
-            self.introduction = SelfIntroducePrompt(agent=self).invoke(self.model)
-        return self.introduction
+        if self._introduction == '' or update:
+            self._introduction = SelfIntroducePrompt(agent=self).invoke(self._model)
+        return self._introduction
 
     def grant_ability(self, ability: Callable, update_introduction: bool = True):
-        self.abilities.append(Ability(ability))
+        self._abilities.append(Ability(ability))
         self.introduce(update_introduction)
 
     def grant_abilities(self, abilities: list[Callable]):
@@ -74,9 +67,9 @@ class Agent:
 
     def deprive_ability(self, ability: Callable, update_introduction: bool = True):
         ability = Ability(ability)
-        for _ability in self.abilities:
+        for _ability in self._abilities:
             if _ability.name == ability.name:
-                self.abilities.remove(_ability)
+                self._abilities.remove(_ability)
         self.introduce(update_introduction)
 
     def deprive_abilities(self, abilities: list[Callable]):
@@ -85,38 +78,38 @@ class Agent:
         self.introduce(update=True)
 
     def plan(self, _log: bool = True) -> list:
-        scheduling = SchedulerPrompt(query=self.query_by_step, abilities=self.abilities)
-        self.schedule = scheduling.invoke(self.model)
+        scheduling = SchedulerPrompt(query=self._query_by_step, abilities=self._abilities)
+        self.__schedule = scheduling.invoke(self._model)
         if _log:
-            log.debug(f'Agent: {self.name}, Schedule: {[_.name for _ in self.schedule]}. Query: "{self.query_high_level}".')
-        return self.schedule
+            log.debug(f'Agent: {self._name}, Schedule: {[_.name for _ in self.__schedule]}. Query: "{self._query_high_level}".')
+        return self.__schedule
 
     def reposition(self):
-        self.prev_results = list()
-        self.schedule = list()
-        self.act_count = 0
+        self.__prev_results = list()
+        self.__schedule = list()
+        self._act_count = 0
         return self
 
     def assign(self, query: str):
-        self.query_high_level, self.query_by_step = (
-            QueryResolverPrompt(query=query).invoke(self.model))
+        self._query_high_level, self._query_by_step = (
+            QueryResolverPrompt(query=query).invoke(self._model))
         return self.reposition()
 
     def reassign(self, query: str):
         return self.assign(query)
 
-    def step_quiet(self) -> str:
-        if self.act_count < len(self.schedule):
+    def __step_quiet(self) -> str:
+        if self._act_count < len(self.__schedule):
             analysing = AnalyserPrompt(
-                query=self.query_by_step,
-                prev_results=self.prev_results,
-                action=self.schedule[self.act_count]
+                query=self._query_by_step,
+                prev_results=self.__prev_results,
+                action=self.__schedule[self._act_count]
             )
-            action, params = analysing.invoke(self.model)
+            action, params = analysing.invoke(self._model)
             executing = ExecutorPrompt(params=params, action=action)
-            action_str = f'Agent: {self.name}, Action {self.act_count + 1}/{len(self.schedule)}: {executing.invoke(model=self.model)}'
-            self.prev_results.append(action_str)
-            self.act_count += 1
+            action_str = f'Agent: {self._name}, Action {self._act_count + 1}/{len(self.__schedule)}: {executing.invoke(model=self._model)}'
+            self.__prev_results.append(action_str)
+            self._act_count += 1
             log.debug(action_str)
             return action_str
         self.reposition()
@@ -125,12 +118,12 @@ class Agent:
     def just_do_it(self) -> str | None:
         if not self.plan():
             return None
-        for act_count in range(len(self.schedule)):
-            self.step_quiet()
+        for act_count in range(len(self.__schedule)):
+            self.__step_quiet()
         response = IntrospectionPrompt(
-            query=self.query_high_level,
-            prev_results=self.prev_results,
-        ).invoke(self.model)
-        log.debug(f'Agent: {self.name}, Conclusion: {response}')
+            query=self._query_high_level,
+            prev_results=self.__prev_results,
+        ).invoke(self._model)
+        log.debug(f'Agent: {self._name}, Conclusion: {response}')
         self.reposition()
-        return f'{self.name}: {response}'
+        return f'{self._name}: {response}'
