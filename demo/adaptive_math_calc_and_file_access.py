e01b7ed246672ab2b0cd836f5bbbd77a22c85fc5
吴子豪 <vortezwohl@proton.me>
2024-12-01 07:31:59 +0800

    添加新测试用例，用于测试adaptive_agent

---
+++ demo/adaptive_math_calc_and_file_access.py
@@ -0,0 +1,27 @@
+from ceo import AdaptiveAgent, get_openai_model
+from sympy import simplify
+from dotenv import load_dotenv
+
+from ceo.util import ability
+
+load_dotenv()
+
+
+@ability
+def calculator(expr: str) -> float:
+    # this function only accepts a single math expression
+    return simplify(expr)
+
+
+@ability
+def write_file(filename: str, content: str) -> bool:
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(content)
+    return True
+
+
+if __name__ == '__main__':
+    ceo = AdaptiveAgent(abilities=[calculator, write_file], brain=get_openai_model(), name='test', p=0.5, beta=0.5)
+    ceo.assign("Here is a sphere with radius of (1 * 9.5 / 2 * 2) cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
+    result = ceo.just_do_it()
+    print(result)
