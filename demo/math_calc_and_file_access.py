e394879867c081c9375ec6f0c23dc95acc8abbea
吴子豪 <vortezwohl@proton.me>
2024-12-03 01:57:47 +0800

    update demo

--- demo/math_calc_and_file_access.py
+++ demo/math_calc_and_file_access.py
@@ -27,7 +27,7 @@ def write_file(filename: str, content: str) -> str:
 
 
 if __name__ == '__main__':
-    ceo = Agent(abilities=[calculator, write_file], brain=get_openai_model(), name='Copilot', personality=Personality.INQUISITIVE)
+    ceo = Agent(abilities=[calculator, write_file], brain=get_openai_model(), name='CEO', personality=Personality.INQUISITIVE)
     ceo.assign("Here is a sphere with radius of (1 * 9.5 / 2 * 2) cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
     result = ceo.just_do_it()
     print(result)
--- demo/multi_agent.py
+++ demo/multi_agent.py
@@ -28,18 +28,22 @@ def write_file(filename: str, content: str) -> str:
     return f'{content} written to {filename}.'
 
 
-@agentic(Agent(abilities=[calculator], brain=model, name='Jack', personality=Personality.INQUISITIVE))
+jack = Agent(abilities=[calculator], brain=model, name='Jack', personality=Personality.INQUISITIVE)
+tylor = Agent(abilities=[write_file], brain=model, name='Tylor', personality=Personality.PRUDENT)
+
+
+@agentic(jack)
 def agent1():
     return
 
 
-@agentic(Agent(abilities=[write_file], brain=model, name='Tylor', personality=Personality.PRUDENT))
+@agentic(tylor)
 def agent2():
     return
 
 
 if __name__ == '__main__':
-    ceo = Agent(abilities=[agent1, agent2], brain=model, name='Copilot', personality=Personality.INQUISITIVE)
+    ceo = Agent(abilities=[agent1, agent2], brain=model, name='CEO', personality=Personality.INQUISITIVE)
     ceo.assign("Here is a sphere with radius of (1 * 9.5 / 2 * 2) cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
     result = ceo.just_do_it()
     print(result)
