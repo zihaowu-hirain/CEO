67861905525a77008eccf479fea167a5dec6b7d2
吴子豪 <vortezwohl@proton.me>
2024-12-03 00:53:42 +0800

    update demo

--- demo/math_calc_and_file_access.py
+++ demo/math_calc_and_file_access.py
@@ -1,6 +1,11 @@
 import logging
 
-from ceo import Agent, get_openai_model, ability
+from ceo import (
+    Agent,
+    Personality,
+    get_openai_model,
+    ability
+)
 from sympy import simplify
 from dotenv import load_dotenv
 
@@ -15,14 +20,14 @@ def calculator(expr: str) -> float:
 
 
 @ability
-def write_file(filename: str, content: str) -> bool:
+def write_file(filename: str, content: str) -> str:
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
-    return True
+    return f'{content} written to {filename}.'
 
 
 if __name__ == '__main__':
-    ceo = Agent(abilities=[calculator, write_file], brain=get_openai_model(), name='Copilot')
+    ceo = Agent(abilities=[calculator, write_file], brain=get_openai_model(), name='Copilot', personality=Personality.INQUISITIVE)
     ceo.assign("Here is a sphere with radius of (1 * 9.5 / 2 * 2) cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
     result = ceo.just_do_it()
     print(result)
--- demo/multi_agent.py
+++ demo/multi_agent.py
@@ -2,7 +2,13 @@ import logging
 
 from sympy import simplify
 from dotenv import load_dotenv
-from ceo import Agent, get_openai_model, agentic, ability
+from ceo import (
+    Agent,
+    Personality,
+    get_openai_model,
+    agentic,
+    ability
+)
 
 load_dotenv()
 logging.getLogger('ceo').setLevel(logging.DEBUG)
@@ -22,19 +28,18 @@ def write_file(filename: str, content: str) -> str:
     return f'{content} written to {filename}.'
 
 
-@agentic(Agent(abilities=[calculator], brain=model, name='Jack'))
+@agentic(Agent(abilities=[calculator], brain=model, name='Jack', personality=Personality.INQUISITIVE))
 def agent1():
     return
 
 
-@agentic(Agent(abilities=[write_file], brain=model, name='Tylor'))
+@agentic(Agent(abilities=[write_file], brain=model, name='Tylor', personality=Personality.PRUDENT))
 def agent2():
     return
 
 
 if __name__ == '__main__':
-    ceo = Agent(abilities=[agent1, agent2], brain=model, name='Copilot')
-    ceo.assign(
-        "Here is a sphere with radius of (1 * 9.5 / 2 * 2) cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
+    ceo = Agent(abilities=[agent1, agent2], brain=model, name='Copilot', personality=Personality.INQUISITIVE)
+    ceo.assign("Here is a sphere with radius of (1 * 9.5 / 2 * 2) cm and pi here is 3.14159, find the area and volume respectively then write the results into a file called 'result.txt'.")
     result = ceo.just_do_it()
     print(result)
