89bc914a17a99e8313b7f4604ec8b8cd3bde3183
Vortez Wohl / 吳子豪 <117743023+vortezwohl@users.noreply.github.com>
2024-12-17 17:36:55 +0800

    Update prompt.py

--- ceo/prompt/prompt.py
+++ ceo/prompt/prompt.py
@@ -5,7 +5,7 @@ from langchain_core.language_models import BaseChatModel
 
 class Prompt:
     def __init__(self, prompt: str, ext_context: str = ''):
-        self.seperator = f'\n{'-'*5}You are my assistant{'-'*5}\n'
+        self.seperator = f"\n{'-'*5}You are my assistant{'-'*5}\n"
         self.ext_context = ext_context
         self.prompt = f'{self.ext_context}{self.seperator}{prompt}'
 
