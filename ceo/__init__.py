b36760afe82e79f3247b3a0568f017a6cca61a8e
吴子豪 <vortezwohl@proton.me>
2024-12-26 01:00:08 +0800

    更新版本到 0.9.2-preview

--- ceo/__init__.py
+++ ceo/__init__.py
@@ -8,7 +8,7 @@ from .enum import Personality
 
 __AUTHOR__ = '吴子豪 / Vortez Wohl'
 __EMAIL__ = 'vortez.wohl@gmail.com'
-__VERSION__ = '0.9.0-preview'
+__VERSION__ = '0.9.2-preview'
 
 logger = logging.getLogger('ceo')
 logger.setLevel(logging.INFO)
