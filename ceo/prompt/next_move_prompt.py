bba46692581cb7df0312141359a6ec32f216fe10
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2024-12-25 14:51:01 +0800

    update: 优化next move prompt性能

--- ceo/prompt/next_move_prompt.py
+++ ceo/prompt/next_move_prompt.py
@@ -8,7 +8,9 @@ from ceo.prompt.prompt import Prompt
 
 log = logging.getLogger('ceo.prompt')
 
-SEPARATOR = "----SEPARATOR----"
+SEPARATOR = '--SEP--'
+MISSION_COMPLETE = '-mission-complete-'
+MISSION_FAILED = '-mission-failed-'
 
 OUTPUT_EXAMPLE = """
 Step 1: In the provided history, the events related to the user's query are as follows, listed chronologically:
@@ -64,50 +66,50 @@ class NextMovePrompt(Prompt):
             history = "Nothing happened before you."
         prompt = json.dumps({
             "precondition": "Below are the abilities you have(you can only use the following abilities)."
-                            "[history] shows events happened before you. And there is a [user_query].",
+                            "<history> shows events happened before you. And there is a <user_query>.",
             "user_query": f'"{query}"',
             "abilities": json.dumps(abilities_dict, ensure_ascii=False),
             "history": history,
             "instructions_you_must_follow_step_by_step": [{
                     "step": 1,
-                    "first_action": "List all events in the [history] related to [user_query] respectively and chronologically.",
-                    "second_action": "Extract and list all key information related to [user_query] from [history] "
+                    "first_action": "List all events in the <history> related to <user_query> respectively and chronologically.",
+                    "second_action": "Extract and list all key information related to <user_query> from <history> "
                                      "formatted one by one respectively.",
-                    "additional": "For any details mentioned in [history] about [user_query], you should preserve them in full, "
+                    "additional": "For any details mentioned in <history> about <user_query>, you should preserve them in full, "
                                   "especially specific information with accuracy requirements such as numbers, dates, etc."
                 }, {
                     "step": 2,
-                    "action": "Analyse whether the [user_query] has been fully and properly accomplished "
-                              "according to your report in [step_1], "
+                    "action": "Analyse whether the <user_query> has been fully and properly accomplished "
+                              "according to your report in <step_1>, "
                               "and provide your analysis process and basis."
                 }, {
                     "step": 3,
-                    "condition": "If the [user_query] has not been fully properly accomplished",
-                    "action": "Analyse whether your [abilities] can complete the unfinished part of the [user_query], "
-                              "and provide the basis according to your report in [step_1]."
+                    "condition": "If the <user_query> has not been fully properly accomplished",
+                    "action": "Analyse whether your <abilities> can complete the unfinished part of the <user_query>, "
+                              "and provide the basis according to your report in <step_1>."
                 }, {
                     "step": 4,
-                    "condition": "If the [user_query] has not been fully properly accomplished and "
-                                 "there is an ability in your [abilities] "
-                                 "that can further advance the accomplishment of the [user_query] based on [history].",
-                    "first_action": "Plan and explain your next move based on [history] "
-                                    "for further advancing the [user_query].",
+                    "condition": "If the <user_query> has not been fully properly accomplished and "
+                                 "there is an ability in your <abilities> "
+                                 "that can further advance the accomplishment of the <user_query> based on <history>.",
+                    "first_action": "Plan and explain your next move based on <history> "
+                                    "for further advancing the <user_query>.",
                     "second_action": "Choose and provide the ability according to your next move"
                                      "(only one single ability can be chosen)",
                     "third_action": "After you have chosen the ability as next move, "
-                                    "generate values of parameters for the ability(function) to achieve [next move], "
+                                    "generate values of parameters for the ability(function) to achieve <next move>, "
                                     "before you generate values of parameters, explain why you give these values to params.",
                 }, {
                     "step": 5,
-                    "condition": "If the [user_query] has not been fully properly accomplished and "
-                                 "there is no ability in your [abilities] "
-                                 "that can further advance the accomplishment of the [user_query]",
-                    "action": 'Provide a special ability called "-mission-failed-" (which is not a real ability).'
+                    "condition": "If the <user_query> has not been fully properly accomplished and "
+                                 "there is no ability in your <abilities> "
+                                 "that can further advance the accomplishment of the <user_query>",
+                    "action": f'Provide a special ability called "{MISSION_FAILED}" (which is not a real ability).'
                 }, {
                     "step": 6,
-                    "condition": "If the [user_query] has been fully and properly accomplished "
-                                 "according to [history] and [user_query]",
-                    "action": 'Provide a special ability called "-mission-complete-" (which is not a real ability).'
+                    "condition": "If the <user_query> has been fully and properly accomplished "
+                                 "according to <history> and <user_query>",
+                    "action": f'Provide a special ability called "{MISSION_COMPLETE}" (which is not a real ability).'
                 }
             ],
             "output_format": "{step1_thought_process}\n{step2_thought_process}\n"
@@ -123,13 +125,13 @@ class NextMovePrompt(Prompt):
             "hint_for_thought_process_output": "Thought processes of all steps(from 1 to 6) should be output.",
             "hint_for_ability_choosing": "Only one single ability can be chosen.",
             "hint_for_params_output_format": f'The "{SEPARATOR}" pattern should be after '
-                                             f'all the thought processes and before the [params and ability].'
+                                             f'all the thought processes and before the <params and ability>.'
                                              f'The params should be after the "{SEPARATOR}" pattern.'
                                              'The params should be formatted as json.'
                                              'The params only gives the params for the chosen one ability.'
                                              'The ability should be after params.',
-            "hint_for_separation_pattern": f'The "{SEPARATOR}" pattern which separates [thought processes] and '
-                                           '[params, ability] is absolutely important, do not forget to place it.',
+            "hint_for_separation_pattern": f'The "{SEPARATOR}" pattern which separates <thought processes> and '
+                                           '<params and ability> is absolutely important, do not forget to place it.',
             "hint_for_ability_output_format": 'The ability should be after the params. '
                                               'The ability name should be surrounded by "[ ]".',
             "output_example": OUTPUT_EXAMPLE
@@ -143,8 +145,8 @@ class NextMovePrompt(Prompt):
         result = result[result.rfind(SEPARATOR):]
         params: dict = json.loads(result[result.find('{'):result.rfind('}') + 1].strip())
         result = result[result.rfind('}') + 1:]
-        ability_name: str = result[result.rfind('['):result.rfind(']') + 1].strip()[1:-1]
-        if ability_name.__contains__('-mission-complete-'):
+        ability_name: str = result[result.find('['):result.rfind(']') + 1].strip()[1:-1]
+        if ability_name.__contains__(MISSION_COMPLETE):
             return True
         for ability in self.abilities:
             if ability.name == ability_name:
