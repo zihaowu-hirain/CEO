39a9ebc033a79004a9fd93f2b04aba654eb2f88a
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2024-12-26 11:24:49 +0800

    fix: 解决 tooDumpException 模板字符串报错

--- ceo/exception/too_dumb_exception.py
+++ ceo/exception/too_dumb_exception.py
@@ -3,5 +3,6 @@ from langchain_core.language_models import BaseChatModel
 
 class TooDumbException(Exception):
     def __init__(self, model: BaseChatModel):
-        super().__init__(f'The model "{model.dict()['model_name']}" is too dumb to be my brain, '
+        model_name = model.dict()['model_name']
+        super().__init__(f'The model "{model_name}" is too dumb to be my brain, '
                          f'swap it or change its temperature.')
