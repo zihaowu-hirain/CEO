e68a987ac6c04568925a2b1ec87cb0a461f9a028
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-01-03 18:07:46 +0800

    feat: 添加 dashscope 支持

--- ceo/brain/lm/dashscope.py
+++ ceo/brain/lm/dashscope.py
@@ -5,16 +5,18 @@ from langchain_core.language_models import BaseChatModel
 
 DEFAULT_TMP = 0.125
 DEFAULT_TOP_P = 1.00
-DEFAULT_GPT = 'gpt-4o-mini'
-API_KEY = os.environ.get('OPENAI_API_KEY', None)
+DEFAULT_QWEN = 'qwen-max'
+API_KEY = os.environ.get('DASHSCOPE_API_KEY', None)
+BASE_URL = 'https://dashscope.aliyuncs.com/compatible-mode/v1'
 
 
-def get_lm(key: str = API_KEY, name: str = DEFAULT_GPT, temp: float = DEFAULT_TMP, top_p: float = DEFAULT_TOP_P,
+def get_lm(key: str = API_KEY, name: str = DEFAULT_QWEN, temp: float = DEFAULT_TMP, top_p: float = DEFAULT_TOP_P,
            stream: bool = False) -> BaseChatModel:
     return langchain_openai.ChatOpenAI(
         api_key=key,
         model=name,
         top_p=top_p,
         temperature=temp,
-        streaming=stream
+        streaming=stream,
+        base_url=BASE_URL
     )
