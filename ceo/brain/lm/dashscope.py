aea70bf93c27e9fbc1f34e51b38e20cfc35d8fcc
吴子豪 <vortezwohl@proton.me>
2025-01-04 17:12:21 +0800

    update dashscope.py

--- ceo/brain/lm/dashscope.py
+++ ceo/brain/lm/dashscope.py
@@ -8,8 +8,8 @@ DEFAULT_QWEN = 'qwen-plus'
 
 def get_lm(key: str = None, name: str = DEFAULT_QWEN, temp: float = DEFAULT_TMP, top_p: float = DEFAULT_TOP_P,
            stream: bool = False) -> BaseChatModel:
-    return langchain_community.chat_models.ChatTongyi(
-        api_key=key,
+    return langchain_community.chat_models.tongyi.ChatTongyi(
+        dashscope_api_key=key,
         model=name,
         top_p=top_p,
         temperature=temp,
