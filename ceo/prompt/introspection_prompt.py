8fef6393d9892f690875799c31b56e819676e378
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-01-06 16:59:58 +0800

    优化 introspection_prompt.py

--- ceo/prompt/introspection_prompt.py
+++ ceo/prompt/introspection_prompt.py
@@ -12,22 +12,18 @@ log = logging.getLogger('ceo.prompt')
 class IntrospectionPrompt(Prompt):
     def __init__(self, query: str, prev_results: list | str, ext_context: str = ''):
         prompt = json.dumps({
-            "precondition": "Below are actions you (you are the bot/assistant) have performed "
-                            "to achieve the <user_query>. "
-                            "You are talking to the user (I (who now talking to you here) am the user), "
-                            "use 'you' instead of the 'user' when you organize your response (output).",
+            "precondition": "Below in <history> are actions have been performed to achieve <user_query>. ",
             "user_query": query,
-            "task": "Tell user's intention first, "
-                    "then think seriously whether you have achieved the user's query "
-                    "according to actions you have performed. "
-                    "Finally, provide the results wanted by the user based on <user_query>. "
-                    "If you did not achieve the user's query, explain why?",
+            "task": "Think seriously whether <user_query> has been fully achieved "
+                    "according to <history>. "
+                    "Then, provide the detailed results mentioned in <history> accurately. "
+                    "Finally, if the <user_query> has not been fully achieved, explain why failed?",
             "output_format": "text",
-            "output_example": "Your intention is to calculate math, and I was trying to open calculator. "
+            "output_example": "Your intention is to do calculation, and I was trying to open calculator. "
                               "But I failed because I did not have that ability to open calculator. "
                               "I have not achieved your intention.",
             "hint_for_output": "Your output should be concise and comprehensive",
-            "actions_performed": prev_results if isinstance(prev_results, str) else str(prev_results)
+            "history": prev_results if isinstance(prev_results, str) else str(prev_results)
         }, ensure_ascii=False)
         super().__init__(prompt, ext_context)
         log.debug(f'IntrospectionPrompt: {self.prompt}')
