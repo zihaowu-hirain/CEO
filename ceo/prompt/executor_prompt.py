01070637a579c506b976026d7d7eaf3d7aaff64f
吴子豪(Zihao Wu) <zihao.wu1@hirain.com>
2025-01-07 14:10:10 +0800

    优化 executor_prompt.py

--- ceo/prompt/executor_prompt.py
+++ ceo/prompt/executor_prompt.py
@@ -41,6 +41,7 @@ class ExecutorPrompt(Prompt):
         tmp_params = self.params.copy()
         if self.action.name.startswith(AGENTIC_ABILITY_PREFIX):
             del tmp_params['memory']
+            tmp_params['choice'] = 'Ask for a favor.'
         prompt = json.dumps({
             "precondition": "Below is an ability shown at <ability>, "
                             "your choice(params) for the <ability> is shown at <params(choice)>, "
